76ac947ffdaa1366c4f063b029152a90e8f87e8b
caprosset <capucinerosset@gmail.com>
2020-06-07 21:35:06 +0200

    Set up express server

--- app.js
+++ app.js
@@ -1,41 +1,88 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const cors = require('cors');
+require('dotenv').config();
 var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
+
+// ROUTERS
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
+// MONGOOSE CONNECTION
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    keepAlive: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then( () => console.log('Connected to the DataBase'))
+  .catch( (err) => console.log(err));
+
+
+// EXPRESS SERVER INSTANCE
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
+// CORS MIDDLEWARE SETUP
+app.use(
+  cors({
+    credentials: true,
+    origin: [process.env.PUBLIC_DOMAIN]
+  })
+)
+
+// SESSION MIDDLEWARE
+app.use(
+  session({
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 * 30 * 6, // 6 months
+    }),
+    secret: process.env.SECRET_SESSION,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    }
+  })
+)
+
+// MIDDLEWARES
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// ROUTER MIDDLEWARES
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+// ERROR HANDLING
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  res.status(404).json({ code: 'not found' });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    const statusError = err.status || '500';
+    res.status(statusError).json(err);
+  }
 });
 
 module.exports = app;
