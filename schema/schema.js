d44b5ced0e09346ea283255d8a3accb25f05fe23
caprosset <capucinerosset@gmail.com>
2020-06-09 22:23:01 +0200

    Install lodash library

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,6 @@
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const _ = require('lodash');
 
 const TaskType = new GraphQLObjectType({
   name: 'Task',
@@ -18,10 +19,11 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         // code to get data from db / other source
+        return _.find(tasks, { id: args.id })
       }
     }
   } 
-})  
+}); 
 
 module.exports = new GraphQLSchema({
   query: RootQuery
