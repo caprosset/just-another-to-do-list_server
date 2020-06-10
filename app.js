4c4c2f014834b3e57f8761cfbec08431a2fdba50
caprosset <capucinerosset@gmail.com>
2020-06-10 14:15:24 +0200

    Add Mongoose models and keep implementing GraphQL

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql')
+// const schema = require('./graphql/schema');
+const mongoose = require('mongoose');
+
 const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema');
-const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
+
 const cors = require('cors');
 require('dotenv').config();
 
-
-// ROUTERS
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+const Task = require('./models/task');
 
 
 // MONGOOSE CONNECTION
@@ -24,7 +24,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then( () => console.log('Connected to the DataBase'))
+  .then( () => console.log(`Connected to the DataBase ${process.env.MONGODB_URI}`))
   .catch( (err) => console.log(err));
 
 
@@ -65,13 +65,67 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // ROUTER MIDDLEWARES
-app.use('/graphql', graphqlHTTP({
-  // options
-  schema //schema: schema
-}));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/graphql', graphqlHTTP({ 
+  // configuration of graphQL API
+  schema: buildSchema(`
+    type Task {
+      _id: ID!
+      title: String!
+      description: String!
+      date: String!
+    }
+
+    input TaskInput {
+      title: String!
+      description: String!
+      date: String
+    }
+
+    type RootQuery {
+      tasks: [Task!]!
+    }
+
+    type RootMutation {
+      createTask(taskInput: TaskInput): Task 
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    } 
+  `),
+  rootValue: { 
+    tasks: () => {
+      return Task.find()
+      .then(tasks => {
+        return tasks.map(task => {
+          return task;
+        });
+      })
+      .catch(err => {
+        throw err;
+      })
+    },
+    createTask: args => {
+      const task = new Task({
+        title: args.taskInput.title,
+        description: args.taskInput.description,
+        date: new Date(args.taskInput.date),
+      })
+      return task.save()
+      .then( newTask => {
+        // console.log(newTask);
+        return newTask;
+      }).catch( err => {
+        // console.log(err);
+        throw err;
+      }); 
+      
+    }
+  },
+  graphiql: true
+}));
 
 
 // ERROR HANDLING
